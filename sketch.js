eec642e60fd7fb41e098e8489937c164292e28b0
Irelovecoding <71947565+Irelovecoding@users.noreply.github.com>
2020-12-06 14:42:55 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+const Mouse = Matter.Mouse;
+
+function setup() {
+    canvas = createCanvas(1000,1000);
+	engine = Engine.create();
+	world = engine.world;
+    
+
+    let canvasmouse = Mouse.create(canvas.elt);
+    canvasmouse.pixelRatio = pixelDensity();
+    let options = {
+        mouse: canvasmouse
+    };
+    mConstraint = Matter.MouseConstraint.create(engine, options)
+
+    World.add(world, mConstraint);
+    
+    pendulum1 = new pendulum(340,450,"blue");
+    pendulum2 = new pendulum(400,450,"green");
+    pendulum3 = new pendulum(460,450,"red");
+    pendulum4 = new pendulum(520,450,"yellow");
+    pendulum5 = new pendulum(580,450,"blue");
+    sling1 = new sling(pendulum1.body,{x: 340,y: 200});
+    sling2 = new sling(pendulum2.body,{x: 400,y: 200});
+    sling3 = new sling(pendulum3.body,{x: 460,y: 200});
+    sling4 = new sling(pendulum4.body,{x: 520,y: 200});
+    sling5 = new sling(pendulum5.body,{x: 580,y: 200});
+    Engine.run(engine);
+}
+
+function draw() {
+    background("black");
+    pendulum1.display();
+    pendulum2.display();
+    pendulum3.display();
+    pendulum4.display();
+    pendulum5.display();
+    sling1.display();
+    sling2.display();
+    sling3.display();
+    sling4.display();
+    sling5.display();
+
+}
+
+function mouseDragged() {
+    Matter.Body.setPosition(pendulum1.body, {x: mouseX, y: mouseY});
+}
